45778d26b24171cdcf522dc0ca5890ec82932500
Alexander Nuykin <scepticnav@gmail.com>
2022-05-23 19:45:14 +0300

    Add Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,42 @@
+﻿/*
+Задача 23: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27 
+5 -> 1, 8, 27, 64, 125
+*/
+
+void GetCoube(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        Console.Write($"{Math.Pow(i, 3)}");
+        if (i < n) Console.Write(", ");
+    }
+}
+
+int GetNumber(string msg)
+{
+    while (true)
+    {
+        Console.WriteLine(msg);
+        string valueFromConsole = Console.ReadLine();
+
+        if (int.TryParse(valueFromConsole, out int number))
+        {
+            if (number > 0)
+                return number;
+            else
+            {
+                Console.WriteLine("Число должно быть больше 0. Введите другое число");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Вы ввели не число. Нужно ввести число, отличное от нуля.");
+        }
+    }
+}
+
+string message = "Введите натуральное число больше 0,  N = ";
+int num = GetNumber(message);
+
+GetCoube(num);
